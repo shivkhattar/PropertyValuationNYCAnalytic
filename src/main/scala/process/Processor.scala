5a2ba03845a5321f2f935923cf2e4bb96bd1650a
Rutvik215 <37913706+Rutvik215@users.noreply.github.com>
2020-04-23 20:15:37 -0400

    property data preprocessing final

--- src/main/scala/process/Processor.scala
+++ src/main/scala/process/Processor.scala
@@ -11,20 +11,20 @@ object Processor {
     var now = System.currentTimeMillis()
 
     val cleanedPlutoPath = path + CLEANED_PLUTO_PATH
-    val plutoRDDAndData = PlutoProcess.normalizeLatLongFromRDD(sc, cleanedPlutoPath)
+//    val plutoRDDAndData = PlutoProcess.normalizeLatLongFromRDD(sc, cleanedPlutoPath)
     println("Pluto preprocessing took: " + (System.currentTimeMillis() - now) + " msecs")
 
-    val plutoRDD = plutoRDDAndData._1;
-    val plutoData = plutoRDDAndData._2;
+//    val plutoRDD = plutoRDDAndData._1;
+//    val plutoData = plutoRDDAndData._2;
     now = System.currentTimeMillis()
     val cleanedSubwayPath = path + CLEANED_SUBWAY_PATH
-    val subwayRDD = SubwayProcess.process(sc, cleanedSubwayPath, plutoRDD, plutoData)
+//    val subwayRDD = SubwayProcess.process(sc, cleanedSubwayPath, plutoRDD, plutoData)
 
     val cleanedCrimePath = path + CLEANED_CRIME_PATH + "2"
-    val crimeRDD = CrimeProcess.process(sc, cleanedCrimePath, plutoRDD, plutoData)
+//    val crimeRDD = CrimeProcess.process(sc, cleanedCrimePath, plutoRDD, plutoData)
 
     val cleanedEducationPath = path + CLEANED_EDUCATION_PATH
-    val educationRDD = EducationProcess.process(sc, cleanedEducationPath, plutoRDD, plutoData)
+//    val educationRDD = EducationProcess.process(sc, cleanedEducationPath, plutoRDD, plutoData)
 
     val cleanedPropertyPath = path + CLEANED_PROPERTY_PATH
     val propertyRDD = PropertyProcess.process(sc, cleanedPropertyPath)
@@ -33,6 +33,7 @@ object Processor {
     val finalProcessedPath = path + PROCESSED_DATA_PATH
     deleteFolderIfAlreadyExists(hdfs, finalProcessedPath)
 
+/*
     // Joining all RDDs; Final format: ("Borough_Block", "Latitude", "Longitude", "Crime Score", "Subway Score", "Education Score", "Average Property Price")
     val joined = crimeRDD.join(subwayRDD).mapValues(x => (x._1._1, x._2._1, x._1._2))
       .join(educationRDD).map(x => (x._1, (x._2._1._3._1, x._2._1._3._2, x._2._1._1.toString, x._2._1._2.toString, x._2._2._1.toString)))
@@ -40,6 +41,7 @@ object Processor {
 
     val finalRDD = sc.parallelize(FINAL_HEADING).union(joined)
     finalRDD.saveAsTextFile(finalProcessedPath)
+*/
     println("Final processing took: " + (System.currentTimeMillis() - now) + " msecs")
   }
 }
