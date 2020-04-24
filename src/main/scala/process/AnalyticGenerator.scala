31d9e09250780832192e50abca4f1d24af2c1a00
Rutvik215 <37913706+Rutvik215@users.noreply.github.com>
2020-04-24 18:18:08 -0400

    property data preprocessing final

--- src/main/scala/process/AnalyticGenerator.scala
+++ src/main/scala/process/AnalyticGenerator.scala
@@ -3,14 +3,15 @@ package process
 import org.apache.hadoop.fs.FileSystem
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
 import util.CommonConstants.{FINAL_BB_HEADING, FINAL_ZIPCODE_HEADING, PROCESSED_BB_DATA_PATH, PROCESSED_ZIPCODE_DATA_PATH}
 import util.CommonUtil.deleteFolderIfAlreadyExists
 
 object AnalyticGenerator {
 
-  def generateAnalytic(sc: SparkContext, hdfs: FileSystem, path: String, cleanedPlutoPath: String, subwayRDD: RDD[(String, (Double, (String, String)))], crimeRDD: RDD[(String, (Double, (String, String)))], educationRDD: RDD[(String, (Double, (String, String)))], propertyRDD: RDD[(String, Double)]) = {
+  def generateAnalytic(sess: SparkSession, sc: SparkContext, hdfs: FileSystem, path: String, cleanedPlutoPath: String, subwayRDD: RDD[(String, (Double, (String, String)))], crimeRDD: RDD[(String, (Double, (String, String)))], educationRDD: RDD[(String, (Double, (String, String)))], propertyRDD: RDD[(String, Double)]) = {
     var now = System.currentTimeMillis()
-    val BBAnalyticRDD = boroughBlockAnalytic(sc, hdfs, path, subwayRDD, crimeRDD, educationRDD, propertyRDD)
+    val BBAnalyticRDD = boroughBlockAnalytic(sess, sc, hdfs, path, subwayRDD, crimeRDD, educationRDD, propertyRDD)
     println("BB processing took: " + (System.currentTimeMillis() - now) + " msecs")
 
     now = System.currentTimeMillis()
@@ -19,9 +20,17 @@ object AnalyticGenerator {
   }
 
   // Final format: ("Borough_Block", "Crime Score", "Subway Score", "Education Score", "Average Property Price", "Latitude", "Longitude")
-  private def boroughBlockAnalytic(sc: SparkContext, hdfs: FileSystem, path: String, subwayRDD: RDD[(String, (Double, (String, String)))], crimeRDD: RDD[(String, (Double, (String, String)))], educationRDD: RDD[(String, (Double, (String, String)))], propertyRDD: RDD[(String, Double)]) = {
+  private def boroughBlockAnalytic(sess: SparkSession, sc: SparkContext, hdfs: FileSystem, path: String, subwayRDD: RDD[(String, (Double, (String, String)))], crimeRDD: RDD[(String, (Double, (String, String)))], educationRDD: RDD[(String, (Double, (String, String)))], propertyRDD: RDD[(String, Double)]) = {
+    import sess.implicits._
     val processedBBData = path + PROCESSED_BB_DATA_PATH
     deleteFolderIfAlreadyExists(hdfs, processedBBData)
+
+    val crimeDF = crimeRDD.toDF()
+    val subwayDF = subwayRDD.toDF()
+    val educationDF = educationRDD.toDF()
+    val propertyDF = propertyRDD.toDF()
+
+    crimeDF.join(subwayDF).printSchema()
 
     val BBAnalyticRDD = crimeRDD.join(subwayRDD).mapValues(x => (x._1._1, x._2._1, x._1._2))
       .join(educationRDD).mapValues(x => (x._1._1, x._1._2, x._2._1, x._1._3))
--- src/main/scala/process/Processor.scala
+++ src/main/scala/process/Processor.scala
@@ -2,11 +2,12 @@ package process
 
 import org.apache.hadoop.fs.FileSystem
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
 import util.CommonConstants.{CLEANED_CRIME_PATH, CLEANED_EDUCATION_PATH, CLEANED_PLUTO_PATH, CLEANED_PROPERTY_PATH, CLEANED_SUBWAY_PATH}
 
 object Processor {
 
-  def process(sc: SparkContext, hdfs: FileSystem, path: String): Unit = {
+  def process(sess: SparkSession, sc: SparkContext, hdfs: FileSystem, path: String): Unit = {
     val now = System.currentTimeMillis()
 
     val cleanedPlutoPath = path + CLEANED_PLUTO_PATH
@@ -28,6 +29,6 @@ object Processor {
     val cleanedPropertyPath = path + CLEANED_PROPERTY_PATH
     val propertyRDD = PropertyProcess.process(sc, cleanedPropertyPath)
 
-    AnalyticGenerator.generateAnalytic(sc, hdfs, path, cleanedPlutoPath, subwayRDD, crimeRDD, educationRDD, propertyRDD)
+    AnalyticGenerator.generateAnalytic(sess, sc, hdfs, path, cleanedPlutoPath, subwayRDD, crimeRDD, educationRDD, propertyRDD)
   }
 }
